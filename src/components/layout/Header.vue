f48580f23051256a20d240862c9dedf4741153a0
Mads Nylund <madsnyl@gmail.com>
2021-11-17 18:20:11 +0100

    Start on header

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <header>
+        <h1>StarWarsPedia</h1>
+        <ul>
+            <li v-for="nav in navs" :key="nav"></li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return{
+            navs: [
+                {
+                    href: '/',
+                    title: 'persons'
+                },
+                {
+                    href: '/',
+                    title: 'planets'
+                },
+                {
+                    href: '/',
+                    title: 'films'
+                },
+                {
+                    href: '/',
+                    title: 'vehicles'
+                },
+                {
+                    href: '/',
+                    title: 'species'
+                },
+                {
+                    href: '/',
+                    title: 'starships'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+header {
+    width: 100%;
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+}
+</style>
